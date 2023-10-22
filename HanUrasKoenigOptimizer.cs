3f671084c25218085c22762a6a67a4c29f7d7de3
holycrab13 <jan.forberg@hotmail.de>
2023-10-23 01:14:16 +0200

    Update HanUrasKoenigOptimizer.cs

--- HanUrasKoenigOptimizer.cs
+++ HanUrasKoenigOptimizer.cs
@@ -4,6 +4,11 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
+public struct GridNodeSelectionPattern
+{
+    public bool[,] selectionMatrix;
+}
+
 /// <summary>
 /// Path Optimizer based on https://ojs.aaai.org/index.php/SOCS/article/view/18541 by Jihee Han et al.
 /// </summary>
@@ -16,29 +21,45 @@ public class HanUrasKoenigOptimizer : MonoModifier
 
     private GridGraph _grid;
 
+    [SerializeField]
+    private int _cellRetrievalPrecision = 8;
+
+    private GridNodeSelectionPattern[] _patternLookup;
+
     public override void Apply(Path path)
     {
-        if (_grid == null)   
+        if (_grid == null)
         {
             _grid = AstarPath.active.graphs[_graphIndex] as GridGraph;
         }
 
-        if(_grid == null)
+        if (_grid == null)
         {
             return;
         }
 
-        path.vectorPath = DoStringPulling(path.vectorPath);
+        try
+        {
+            path.vectorPath = DoStringPulling(path.vectorPath, _grid);
+        }
+        catch(Exception e)
+        {
+            Debug.LogError(e.Message);
+        }
+
     }
 
-    private List<Vector3> DoStringPulling(List<Vector3> path)
+    private List<Vector3> DoStringPulling(List<Vector3> path, GridGraph grid)
     {
+        List<Vector2Int> sampledCells = new List<Vector2Int>();
         List<Vector2Int> blockedCells = new List<Vector2Int>();
         List<Vector3> shortestPath = new List<Vector3>();
         int turn = 0;
         float[] cornerAngles = new float[4];
 
         shortestPath.Add(path[0]);
+
+        int stuckBro = 0;
 
         for (int i = 2; i < path.Count; i++)
         {
@@ -49,15 +70,24 @@ public class HanUrasKoenigOptimizer : MonoModifier
 
             if (Mathf.Abs(diff.x) > 0.01f && Mathf.Abs(diff.z) > 0.01f)
             {
-                blockedCells.AddRange(FindCells(last, path[i]).Where(c => c == null || IsCellBlocked(c)));
+                sampledCells.Clear();
+                FindCellsWithLookup(sampledCells, last, path[i], grid.nodeSize, grid.width);
+
+                foreach (Vector2Int cell in sampledCells)
+                {
+                    if (IsCellBlocked(cell, grid))
+                    {
+                        blockedCells.Add(cell);
+                    }
+                }
             }
 
-            if (blockedCells.Count() > 0)
+            if (blockedCells.Count > 0)
             {
                 Vector3 nodeToAdd = Vector3.zero;
                 float minAngle = float.MaxValue;
 
-                foreach(Vector2Int blockedCell in blockedCells) 
+                foreach (Vector2Int blockedCell in blockedCells)
                 {
                     FindCornerAngles(cornerAngles, blockedCell, last, path[i - 1]);
 
@@ -89,11 +119,19 @@ public class HanUrasKoenigOptimizer : MonoModifier
                 int endIndex = shortestPath.Count - 1;
                 turn = CalculateTurn(shortestPath[endIndex - 1], shortestPath[endIndex], path[i]);
 
+                stuckBro++;
+
+                if (stuckBro > 1000)
+                {
+                    Debug.LogError("HELP ME, I'M STUCK!");
+                    return shortestPath;
+                }
+
                 i--;
             }
             else
             {
-                if(shortestPath.Count > 1)
+                if (shortestPath.Count > 1)
                 {
                     int endIndex = shortestPath.Count - 1;
                     int currentTurn = CalculateTurn(shortestPath[endIndex - 1], shortestPath[endIndex], path[i]);
@@ -102,9 +140,17 @@ public class HanUrasKoenigOptimizer : MonoModifier
                     {
                         shortestPath.RemoveAt(endIndex);
 
-                        if(shortestPath.Count > 1)
+                        if (shortestPath.Count > 1)
                         {
-                            turn = CalculateTurn(shortestPath[endIndex - 2], shortestPath[endIndex - 1], path[i]); 
+                            turn = CalculateTurn(shortestPath[endIndex - 2], shortestPath[endIndex - 1], path[i]);
+                        }
+
+                        stuckBro++;
+
+                        if (stuckBro > 1000)
+                        {
+                            Debug.LogError("HELP ME, I'M STUCK, TOO!");
+                            return shortestPath;
                         }
 
                         i--;
@@ -114,6 +160,7 @@ public class HanUrasKoenigOptimizer : MonoModifier
         }
 
         shortestPath.Add(path.Last());
+
         return shortestPath;
     }
 
@@ -124,7 +171,7 @@ public class HanUrasKoenigOptimizer : MonoModifier
 
         float angle = Vector2.SignedAngle(v1, v2);
 
-        if(Mathf.Abs(angle) < 0.01f)
+        if (Mathf.Abs(angle) < 0.01f)
         {
             return 0;
         }
@@ -134,24 +181,26 @@ public class HanUrasKoenigOptimizer : MonoModifier
 
     private Vector3 GetCellCorner(Vector2Int cell, int k)
     {
-        if(k == 0)
+        float size = _grid.nodeSize;
+
+        if (k == 0)
         {
-            return new Vector3(cell.x + 0.5f, 0, cell.y + 0.5f);
+            return new Vector3((cell.x + 1.5f) * size, 0, (cell.y + 1.5f) * size);
         }
 
         if (k == 1)
         {
-            return new Vector3(cell.x + 0.5f, 0, cell.y - 0.5f);
+            return new Vector3((cell.x + 1.5f) * size, 0, (cell.y - 0.5f) * size);
         }
 
         if (k == 2)
         {
-            return new Vector3(cell.x - 0.5f, 0, cell.y - 0.5f);
+            return new Vector3((cell.x - 0.5f) * size, 0, (cell.y - 0.5f) * size);
         }
 
         if (k == 3)
         {
-            return new Vector3(cell.x - 0.5f, 0, cell.y + 0.5f);
+            return new Vector3((cell.x - 0.5f) * size, 0, (cell.y + 1.5f) * size);
         }
 
         return Vector3.zero;
@@ -168,91 +217,138 @@ public class HanUrasKoenigOptimizer : MonoModifier
         }
     }
 
-    private bool IsCellBlocked(Vector2Int cell)
+    private bool IsCellBlocked(Vector2Int cell, GridGraph grid)
     {
-        if (!_grid.GetNode(cell.x, cell.y)?.Walkable ?? true)
-        {
-            return true;
-        }
-
-        if (!_grid.GetNode(cell.x - 1, cell.y)?.Walkable ?? true)
-        {
-            return true;
-        }
-
-        if (!_grid.GetNode(cell.x, cell.y - 1)?.Walkable ?? true)
-        {
-            return true;
-        }
-
-        if (!_grid.GetNode(cell.x - 1, cell.y - 1)?.Walkable ?? true)
-        {
-            return true;
-        }
-
-        return false;
+        return !grid.GetNode(cell.x, cell.y)?.Walkable ?? true;
     }
 
-    private List<Vector2Int> FindCells(Vector3 start, Vector3 end)
+    private float DistToCell(Vector3 sample, int v1, int v2)
     {
-        List<Vector2Int> cells = new List<Vector2Int>();
+        Vector3 cellPos = new Vector3(0.5f + v1, 0.0f, 0.5f + v2);
+        return Mathf.Max(Mathf.Abs(sample.x - cellPos.x), Mathf.Abs(sample.z - cellPos.z));
+    }
 
-        var diffX = end.x - start.x;
-        var diffY = end.z - start.z;
-        var stepX = Math.Sign(diffX);
-        var stepY = Math.Sign(diffY);
+    public void InitializePatternLookup(int gridWidth, float precision)
+    {
+        int patternCount = gridWidth * (gridWidth + 1) / 2;
+        _patternLookup = new GridNodeSelectionPattern[patternCount];
 
-        start.x += _grid.nodeSize * 0.5f + stepX * 0.01f;
-        start.z += _grid.nodeSize * 0.5f + stepY * 0.01f;
-        end.x += _grid.nodeSize * 0.5f - stepX * 0.01f;
-        end.z += _grid.nodeSize * 0.5f - stepY * 0.01f;
+        int x = 0;
+        int y = 0;
 
-        // Grid cells are 1.0 X 1.0.
-        int x = (int)Math.Floor(start.x);
-        int z = (int)Math.Floor(start.z);
-
-        // Ray/Slope related maths.
-        // Straight distance to the first vertical grid boundary.
-        var xOffset = end.x > start.x ?
-            (Math.Ceiling(start.x) - start.x) :
-          (start.x - Math.Floor(start.x));
-        // Straight distance to the first horizontal grid boundary.
-        var yOffset = end.z > start.z ?
-            (Math.Ceiling(start.z) - start.z) :
-          (start.z - Math.Floor(start.z));
-        // Angle of ray/slope.
-        var angle = Math.Atan2(-diffY, diffX);
-        // NOTE: These can be divide by 0's, but JS just yields Infinity! :)
-        // How far to move along the ray to cross the first vertical grid cell boundary.
-        var tMaxX = xOffset / Math.Cos(angle);
-        // How far to move along the ray to cross the first horizontal grid cell boundary.
-        var tMaxY = yOffset / Math.Sin(angle);
-        // How far to move along the ray to move horizontally 1 grid cell.
-        var tDeltaX = 1.0 / Math.Cos(angle);
-        // How far to move along the ray to move vertically 1 grid cell.
-        var tDeltaY = 1.0 / Math.Sin(angle);
-
-        // Travel one grid cell at a time.
-        var manhattanDistance = Math.Abs(Math.Floor(end.x) - Math.Floor(start.x)) +
-            Math.Abs(Math.Floor(end.z) - Math.Floor(start.z));
-
-        for (int t = 0; t <= manhattanDistance; ++t)
+        for (int i = 0; i < patternCount; i++)
         {
-            cells.Add(new Vector2Int(x, z));
+            _patternLookup[i] = new GridNodeSelectionPattern()
+            {
+                selectionMatrix = CreateSelectionMatrix(x, y, precision)
+            };
 
-            // Only move in either X or Y coordinates, not both.
-            if (Math.Abs(tMaxX) < Math.Abs(tMaxY))
+            x++;
+
+            if (x % gridWidth == 0)
             {
-                tMaxX += tDeltaX;
-                x += stepX;
-            }
-            else
-            {
-                tMaxY += tDeltaY;
-                z += stepY;
+                y++;
+                x = y;
             }
         }
+    }
 
-        return cells;
+    private bool[,] CreateSelectionMatrix(int x, int y, float precision)
+    {
+        Vector3 bottomLeft = new Vector3(0.5f, 0.0f, 0.5f);
+        Vector3 topRight = new Vector3(x + 0.5f, 0.0f, y + 0.5f);
+
+        List<Vector2Int> cells = FindCellsSampled(bottomLeft, topRight, precision);
+
+        bool[,] result = new bool[x + 1, y + 1];
+
+        foreach (Vector2Int cell in cells)
+        {
+            result[cell.x, cell.y] = true;
+        }
+
+        return result;
+    }
+
+    public void FindCellsWithLookup(List<Vector2Int> list, Vector3 start, Vector3 end, float nodeSize, int gridWidth)
+    {
+        if (_patternLookup == null)
+        {
+            InitializePatternLookup(gridWidth, _cellRetrievalPrecision);
+        }
+
+        Vector3 diff = (end - start) / nodeSize;
+        int diffX = Mathf.Abs(Mathf.RoundToInt(diff.x));
+        int diffY = Mathf.Abs(Mathf.RoundToInt(diff.z));
+
+        int diffMin = Mathf.Min(diffX, diffY);
+        int diffMax = Mathf.Max(diffX, diffY);
+        bool mirror = diffY > diffX;
+
+        int signX = (int)Mathf.Sign(diff.x);
+        int signY = (int)Mathf.Sign(diff.z);
+
+        int patternIndex = diffMax - diffMin + (2 * gridWidth * diffMin + diffMin - diffMin * diffMin) / 2;
+        GridNodeSelectionPattern pattern = _patternLookup[patternIndex];
+
+        int x = Mathf.FloorToInt(start.x / nodeSize);
+        int y = Mathf.FloorToInt(start.z / nodeSize);
+
+        for (int i = 0; i <= diffMax; i++)
+        {
+            for (int j = 0; j <= diffMin; j++)
+            {
+                if (pattern.selectionMatrix[i, j])
+                {
+                    int stepX = mirror ? signX * j : signX * i;
+                    int stepY = mirror ? signY * i : signY * j;
+
+                    list.Add(new Vector2Int(x + stepX, y + stepY));
+                }
+            }
+        }
+    }
+
+    public List<Vector2Int> FindCellsSampled(Vector3 start, Vector3 end, float precision)
+    {
+        HashSet<Vector2Int> cells = new HashSet<Vector2Int>();
+        Vector3 diff = end - start;
+
+        start += diff * 0.01f;
+        end -= diff * 0.01f;
+
+        diff = end - start;
+
+        var stepCount = precision * Mathf.CeilToInt(Mathf.Abs(diff.x) + Mathf.Abs(diff.z));
+
+        if (stepCount == 0)
+        {
+            stepCount = 1;
+        }
+
+        Vector3 sample = start;
+        Vector3 step = diff / stepCount;
+
+        for (int i = 0; i <= stepCount; i++)
+        {
+            Debug.DrawLine(sample, sample + Vector3.up);
+            int x = Mathf.FloorToInt(sample.x);
+            int y = Mathf.FloorToInt(sample.z);
+
+            for (int m = -1; m <= 1; m++)
+            {
+                for (int n = -1; n <= 1; n++)
+                {
+                    if (DistToCell(sample, x + m, y + n) < 1 - 0.01f)
+                    {
+                        cells.Add(new Vector2Int(x + m, y + n));
+                    }
+                }
+            }
+
+            sample += step;
+        }
+
+        return cells.ToList();
     }
 }
